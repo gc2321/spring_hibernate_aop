7197a063d853c7b113f97e3ee1d9107a448c9cba
G Chan <gchan.email@gmail.com>
2018-05-02 20:36:37 -0700

    added some AOP stuff

--- sprint-demo-aop/src/com/luv2code/aopdemo/MainDemoApp.java
+++ sprint-demo-aop/src/com/luv2code/aopdemo/MainDemoApp.java
@@ -3,6 +3,7 @@ package com.luv2code.aopdemo;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.luv2code.aopdemo.dao.AccountDAO;
+import com.luv2code.aopdemo.dao.MembershipDAO;
 
 public class MainDemoApp {
 
@@ -14,13 +15,15 @@ public class MainDemoApp {
 		// get the bean from spring container
 		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
 		
+		// get membership bean from spring container
+		MembershipDAO theMembershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
 		
 		// call the business method
 		theAccountDAO.addAccount();
 		
-		// do it again
-		theAccountDAO.addAccount();
-				
+		// call the membership business method
+		theMembershipDAO.addSillyMember();
+		
 		// close the context
 		context.close();
 
