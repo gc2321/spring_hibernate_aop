abc078697327fc9369d794790de821f3f3b306ef
G Chan <gchan.email@gmail.com>
2018-05-03 21:13:09 -0700

    more AOP

--- sprint-demo-aop/src/com/luv2code/aopdemo/dao/MembershipDAO.java
+++ sprint-demo-aop/src/com/luv2code/aopdemo/dao/MembershipDAO.java
@@ -9,4 +9,10 @@ public class MembershipDAO {
 			System.out.println(getClass() + ": DOING STUFF: ADDING A MEMBERSHIP ACCOUNT");
 			return true;
 		}
+		
+		public void goToSleep() {
+			
+			System.out.println(getClass() + ": I'm going to sleep now...");
+			
+		}
 }
